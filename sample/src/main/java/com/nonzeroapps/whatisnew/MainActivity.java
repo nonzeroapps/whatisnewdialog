b8b52aa242fb242f723f4e195f30c92ea57e8753
berkayturanci <berkay.turanci@infoline-tr.com>
2017-08-03 10:08:32 +0300

    test commit

--- sample/src/main/java/com/nonzeroapps/whatisnew/MainActivity.java
+++ sample/src/main/java/com/nonzeroapps/whatisnew/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //test
         // Create and show the dialog.
 
         ArrayList<NewFeatureItem> arrayList = new ArrayList<>();
